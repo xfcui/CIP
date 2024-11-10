036ae22fce6ec1449fed2aae3120f041911b764e
shuaicui0607 <147929338+shuaicui0607@users.noreply.github.com>
2024-11-10 20:45:56 +0800

    Update exp12.sh

--- exp12.sh
+++ exp12.sh
@@ -2,7 +2,7 @@
 # This script runs the exp_encoderonly_tsne.ipynb notebook
 
 # Activate your  environment 
-source /path/to/your/venv/bin/activate
+#source /path/to/your/venv/bin/activate
 
-# Run Jupyter Notebook 
-jupyter nbconvert --to notebook --execute --inplace ./exp_encoderonly_tsne.ipynb
+# Run J
+python3 encoderonly_tsne.py
